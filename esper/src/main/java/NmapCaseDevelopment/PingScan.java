afeb45b415317c78bf5d300fb5680c772ecdf560
Pham Nguyen Quang <nguyenquang_pham@fb2.fra-uas.de>
2019-01-09 15:31:33 +0100

    more ideas, will be implemented somehow

--- esper/src/main/java/NmapCaseDevelopment/PingScan.java
+++ esper/src/main/java/NmapCaseDevelopment/PingScan.java
@@ -6,20 +6,35 @@ import Development.LogEventDev;
 
 /*
  * -sn (aka -sP) option in Nmap.
- * TCP SYN SCAN
+ * TCP SYN (-PS:ping -sS:scan)
  *   try icmp echo reply (NOT LOGGED for some reason)
  *   if(no icmp) then try tcp syn ping (typicallt on port 80 and 443).
  *     port up: ACK received, RST send back
- * TCP ACK SCAN
+ * TCP ACK SCAN (-PA:ping -sA:scan)
  *   sending TCP ACK 
  *     RST is sent back from the machine (connection not valid)
- * UDP SCAN
+ * UDP SCAN (-PU:ping -sU:scan)
  *   send UDP pack (typically port 31 338)
  *     host responding: ICMP port unreachable error (TYPE 3 CODE 3)
- *     else: some ICMP message.        
+ *     else: some ICMP message.      
+ * SCTP INIT ping (-sY):
+ *   SCTP: combine characteristic of TCP and UDP  
+ *     SCTP INIT is sent
+ *     if(SCTP ACK) then host online
+ *     qif(ABORT) service is closed
+ * ICMP ECHO ping(-PE):
+ *   simple ICMP type 0
+ *  IP PROTOCOL scan (-sO):
+ *   scan for open protocol:
+ *     ICMP type 3 code 2 ( protocol unreachable): closed
+ *     Other ICMP unreachable errores (type 3, code: 1.3.9.10,13): filtered
+ *     else: open/filtered.
+ *         
  */
 
-public class sPsnScan {
+public class PingScan {
+	private int state = 0;
+
 	public String getStatement() {
 		String log2 = "select * from LogEventDev match_recognize( "
 				+ "measures A as LogEventDev1, B as LogEventDev2 pattern (A B) define A as A.message.contains('PROTO=TCP SPT=80') and A.message.contains('ACK RST'), B as B.message.contains('PROTO=TCP SPT=443') and B.message.contains('ACK RST'))";
@@ -30,7 +45,12 @@ public class sPsnScan {
 		LogEventDev LogEventDev1 = (LogEventDev) Eventmap.get("LogEventDev1");
 		LogEventDev LogEventDev2 = (LogEventDev) Eventmap.get("LogEventDev2");
 		StringBuffer sb = new StringBuffer();
-		sb.append("possible port scan");
+		sb.append("scan detected (-PA -PS)");
+		state = 1;
 		System.out.println(sb.toString());
+	}
+
+	public int getState() {
+		return state;
 	}
 }
